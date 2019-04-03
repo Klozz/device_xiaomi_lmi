2ca6500733ed82bb80dbc6863a995c4524fa1f2e
Bowgo Tsai <bowgotsai@google.com>
2019-04-03 16:44:10 +0800

    lmi: Adding GSI keys
    Bug: 112293933
    Test: build and checks the keys are installed into ramdisk
    Change-Id: I216f3900e9511e0929418af050ff964898d0aea1
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -11,6 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
