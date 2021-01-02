7b8af5551fd3d83052dcd37b808a628be7a357d9
Vishalcj17 <vishalcj@aospa.co>
2021-01-02 13:07:50 -0300

    lmi: Set up device specific dimensions.
    Signed-off-by: Mr Mesquita <mickaelmendes50@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -235,6 +235,10 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_PACKAGES += \
+    LmiFrameworks \
+    LmiSystemUI
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
