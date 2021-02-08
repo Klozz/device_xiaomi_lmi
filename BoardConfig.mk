5b9a0f00406177b0d58932313b69d379afc53037
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 09:05:33 +0100

    lmi: Import compatibility matrix and manifest
    * From lmi-user 11 RKQ1.200826.002 V12.2.4.0.RJKMIXM release-keys
    
    Change-Id: I9537ab00dc8124b3a9ccce5cf4e0a4a7f8f972a8
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,10 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# HIDL
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lmi
 TARGET_RECOVERY_DEVICE_MODULES := libinit_lmi
