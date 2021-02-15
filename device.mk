2d8e3a9b993c5b398f8c9f4e2cb57dfd07e62beb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-16 02:11:23 +0530

    lmi: Build telephony libs
    Change-Id: Ia5eb8c58d0badf90a33e958eee1ff200c9be20d5
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -226,7 +226,16 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Telephony
-TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
-TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
