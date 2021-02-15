2ac50ff01a9a1d2843f4b524c2269ff86cd0728e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-16 01:58:06 +0530

    lmi: Build IPACM libs
    Change-Id: Idd968a3ea54f1f7fda1e3579bdbce95b96349d3a
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -146,6 +146,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libhwbinder.vendor
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
