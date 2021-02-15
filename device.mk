5a1080f6abb36b16ea71adbdb61790cf4bffe550
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-16 02:06:29 +0530

    lmi: Build QTI power AIDL
    Change-Id: I63411ca91515123e6eb847de728672234f580b4c
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -197,6 +197,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
