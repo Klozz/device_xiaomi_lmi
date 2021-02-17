Xtended-Devices/device_xiaomi_juice

lmi: parts: Add FOD service
Change-Id: Ibc4ec25057d260e2dba9c221f0c79e6485ddf56d

parts/Android.mk
@@ -24,6 +24,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.preference_preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
+    vendor.xiaomi.hardware.displayfeature-V1.0-java \
     vendor.xiaomi.hardware.motor-V1.0-java
 
 LOCAL_RESOURCE_DIR := \

parts/src/org/mokee/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.mokee.settings.doze.DozeUtils;
+import org.mokee.settings.fod.FodUtils;
 import org.mokee.settings.popupcamera.PopupCameraUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -34,6 +35,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
+        FodUtils.startService(context);
         PopupCameraUtils.startService(context);
     }
 }

parts/src/org/mokee/settings/fod/FodService.java
@@ -1,0 +1,77 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mokee.settings.fod;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+
+import vendor.xiaomi.hardware.displayfeature.V1_0.IDisplayFeature;
+
+public class FodService extends Service {
+
+    private static final String TAG = "FodService";
+    private static final boolean DEBUG = false;
+    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (Intent.ACTION_SCREEN_ON.equals(action)) {
+                try {
+                    IDisplayFeature mDisplayFeature = IDisplayFeature.getService();
+                    mDisplayFeature.setFeature(0, 0, 2, 255);
+                    mDisplayFeature.setFeature(0, 3, 0, 255);
+                } catch (Exception e) {
+                    // Do nothing
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        registerReceiver();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        this.unregisterReceiver(mIntentReceiver);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void registerReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        this.registerReceiver(mIntentReceiver, filter);
+    }
+}

parts/src/org/mokee/settings/fod/FodUtils.java
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mokee.settings.fod;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+
+public class FodUtils {
+    public static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, FodService.class),
+                UserHandle.CURRENT);
+    }
+}

Klozz/device_xiaomi_lmi

Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

parts/Android.mk
@@ -24,7 +24,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.preference_preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    vendor.xiaomi.hardware.motor-V1.0-java
+    vendor.xiaomi.hardware.motor-V1.0-java \
+    vendor.xiaomi.hardware.displayfeature-V1.0-java
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \

parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,6 +24,7 @@ import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
+import org.lineageos.settings.fod.FodUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -35,5 +36,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
         PopupCameraUtils.startService(context);
+        FodUtils.startService(context);
     }
 }

parts/src/org/lineageos/settings/fod/FodService.java
@@ -1,0 +1,77 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.fod;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+
+import vendor.xiaomi.hardware.displayfeature.V1_0.IDisplayFeature;
+
+public class FodService extends Service {
+
+    private static final String TAG = "FodService";
+    private static final boolean DEBUG = false;
+    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (Intent.ACTION_SCREEN_ON.equals(action)) {
+                try {
+                    IDisplayFeature mDisplayFeature = IDisplayFeature.getService();
+                    mDisplayFeature.setFeature(0, 0, 2, 255);
+                    mDisplayFeature.setFeature(0, 3, 0, 255);
+                } catch (Exception e) {
+                    // Do nothing
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        registerReceiver();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        this.unregisterReceiver(mIntentReceiver);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void registerReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        this.registerReceiver(mIntentReceiver, filter);
+    }
+}

parts/src/org/lineageos/settings/fod/FodUtils.java
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.fod;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+
+public class FodUtils {
+    public static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, FodService.class),
+                UserHandle.CURRENT);
+    }
+}