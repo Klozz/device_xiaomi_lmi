64830b01960886bd47144ab6ce3b65ce9024c45a
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-02-18 12:08:52 +0530

    lmi: build missing BT libs
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -97,6 +97,11 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
+    libbluetooth_qti \
+    libbthost_if \
+    libldacBT_dec \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
