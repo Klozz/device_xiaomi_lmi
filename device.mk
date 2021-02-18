5109527db9f6d68ed800d42d1515c645c9845987
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-02-19 03:38:45 +0530

    lmi: remove WifiResTarget overlay
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -258,8 +258,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworksResTarget \
     LmiFrameworks \
-    LmiSystemUI \
-    WifiResTarget
+    LmiSystemUI
 
 # Net
 PRODUCT_PACKAGES += \
